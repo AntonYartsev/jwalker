9867a613c0667911b8a7475d7168837c07422a51
anton.yartsev <yartsevanton.nsk@gmail.com>
2023-07-11 23:37:29 +0600

    fix

--- index.js
+++ index.js
@@ -7,59 +7,51 @@ const packageDefinition = protoLoader.loadSync('service.proto');
 const Docker = require('dockerode');
 
 const jwalker = grpc.loadPackageDefinition(packageDefinition).jwalker;
-const docker = new Docker({socketPath: '/var/run/docker.sock'});
+const docker = new Docker({ socketPath: '/var/run/docker.sock' });
 
-const args = process.argv.slice(2);
+const host = process.env.GRPC_HOST || '0.0.0.0';
+const port = process.env.GRPC_PORT || '8080';
 
-if (args.length == 0) {
-    console.error("server type identifier not passed")
-} else if (args[0] == 'grpc') {
+const server = new grpc.Server();
+server.addService(jwalker.Parser.service, { parse: parse });
+server.addService(jwalker.System.service, { restart: restart });
 
-    const host = process.env.GRPC_HOST || '0.0.0.0';
-    const port = process.env.GRPC_PORT || '8080';
+server.bindAsync(`${host}:${port}`, grpc.ServerCredentials.createInsecure(), () => {
+    console.log(`Server running at ${host}:${port}`);
+    server.start();
+});
 
-    const server = new grpc.Server();
-    server.addService(jwalker.Parser.service, { parse: parse });
-    server.addService(jwalker.System.service, { restart: restart });
+async function parse(call, callback) {
+    try {
+        var response = ""
+        const browser = await puppeteer.launch({
+            args: ['--no-sandbox'],
+            headless: 'new',
+        });
+        const page = await browser.newPage();
+        await page.goto(call.request.url);
+        const data = await page.evaluate(() => document.querySelector('*').outerHTML);
+        response = data;
+        await browser.close();
+        callback(null, { response: response });
+    } catch (err) {
+        callback(err);
+    }
+}
 
-    server.bindAsync(`${host}:${port}`, grpc.ServerCredentials.createInsecure(), () => {
-        server.start();
-    });
+async function restart(call, callback) {
+    console.log("restart")
+    var containerName = "jwalker"
+    const containers = await docker.listContainers();
+    const containerInfo = containers.find(container => container.Names && container.Names[0] === `/${containerName}`);
 
-    async function parse(call, callback) {
-        try {
-            var response = ""
-            const browser = await puppeteer.launch({
-                args: ['--no-sandbox'],
-                headless: 'new',
-            });
-            const page = await browser.newPage();
-            await page.goto(call.request.url);
-            const data = await page.evaluate(() => document.querySelector('*').outerHTML);
-            response = data;
-            await browser.close();
-            callback(null, { response: response });
-        } catch (err) {
-            callback(err);
-        }
+    if (!containerInfo) {
+        console.error('Container not found');
+        return;
     }
 
-    async function restart(call, callback) {
-        console.log("restart")
-        var containerName = args[1]
-        const containers = await docker.listContainers();
-        const containerInfo = containers.find(container => container.Names && container.Names[0] === `/${containerName}`);
-
-        if (!containerInfo) {
-          console.error('Container not found');
-          return;
-        }
-
-        const container = docker.getContainer(containerInfo.Id);
-        container.restart();
-        callback(null);
-    }
-} else {
-    console.error("unknown server type identifier")
+    const container = docker.getContainer(containerInfo.Id);
+    container.restart();
+    callback(null);
 }
 
